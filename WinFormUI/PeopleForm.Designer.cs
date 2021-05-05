151704bf9e00af7a2e076aa64ae24cd89768571d
Stanciu Mihai <misu_sergiu@yahoo.com>
2021-05-06 00:07:41 +0300

    Added provisory UI elements

--- WinFormUI/PeopleForm.Designer.cs
+++ WinFormUI/PeopleForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PeopleForm));
             this.firstNameLabel = new System.Windows.Forms.Label();
             this.lastNameLabel = new System.Windows.Forms.Label();
             this.firstNameText = new System.Windows.Forms.TextBox();
@@ -37,12 +38,14 @@
             this.listPeopleListBox = new System.Windows.Forms.ListBox();
             this.addPersonButton = new System.Windows.Forms.Button();
             this.refreshListButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // firstNameLabel
             // 
             this.firstNameLabel.AutoSize = true;
-            this.firstNameLabel.Location = new System.Drawing.Point(46, 101);
+            this.firstNameLabel.Location = new System.Drawing.Point(58, 202);
             this.firstNameLabel.Name = "firstNameLabel";
             this.firstNameLabel.Size = new System.Drawing.Size(131, 29);
             this.firstNameLabel.TabIndex = 0;
@@ -51,7 +54,7 @@
             // lastNameLabel
             // 
             this.lastNameLabel.AutoSize = true;
-            this.lastNameLabel.Location = new System.Drawing.Point(46, 190);
+            this.lastNameLabel.Location = new System.Drawing.Point(58, 291);
             this.lastNameLabel.Name = "lastNameLabel";
             this.lastNameLabel.Size = new System.Drawing.Size(128, 29);
             this.lastNameLabel.TabIndex = 1;
@@ -59,72 +62,87 @@
             // 
             // firstNameText
             // 
-            this.firstNameText.Location = new System.Drawing.Point(52, 137);
+            this.firstNameText.Location = new System.Drawing.Point(64, 238);
             this.firstNameText.Name = "firstNameText";
-            this.firstNameText.Size = new System.Drawing.Size(234, 35);
+            this.firstNameText.Size = new System.Drawing.Size(247, 35);
             this.firstNameText.TabIndex = 2;
             // 
             // lastNameText
             // 
-            this.lastNameText.Location = new System.Drawing.Point(52, 226);
+            this.lastNameText.Location = new System.Drawing.Point(64, 327);
             this.lastNameText.Name = "lastNameText";
-            this.lastNameText.Size = new System.Drawing.Size(234, 35);
+            this.lastNameText.Size = new System.Drawing.Size(247, 35);
             this.lastNameText.TabIndex = 3;
             // 
             // addPersonHeader
             // 
             this.addPersonHeader.AutoSize = true;
             this.addPersonHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.addPersonHeader.Location = new System.Drawing.Point(32, 42);
+            this.addPersonHeader.Location = new System.Drawing.Point(24, 142);
             this.addPersonHeader.Name = "addPersonHeader";
-            this.addPersonHeader.Size = new System.Drawing.Size(207, 39);
+            this.addPersonHeader.Size = new System.Drawing.Size(324, 39);
             this.addPersonHeader.TabIndex = 4;
-            this.addPersonHeader.Text = "Add Person";
+            this.addPersonHeader.Text = "Add/Delete Person";
             // 
             // listPeopleHeader
             // 
             this.listPeopleHeader.AutoSize = true;
             this.listPeopleHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.listPeopleHeader.Location = new System.Drawing.Point(380, 42);
+            this.listPeopleHeader.Location = new System.Drawing.Point(450, 142);
             this.listPeopleHeader.Name = "listPeopleHeader";
-            this.listPeopleHeader.Size = new System.Drawing.Size(199, 39);
+            this.listPeopleHeader.Size = new System.Drawing.Size(240, 39);
             this.listPeopleHeader.TabIndex = 5;
-            this.listPeopleHeader.Text = "List People";
+            this.listPeopleHeader.Text = "List of People";
             // 
             // listPeopleListBox
             // 
             this.listPeopleListBox.FormattingEnabled = true;
             this.listPeopleListBox.ItemHeight = 29;
-            this.listPeopleListBox.Location = new System.Drawing.Point(388, 107);
+            this.listPeopleListBox.Location = new System.Drawing.Point(400, 208);
             this.listPeopleListBox.Name = "listPeopleListBox";
             this.listPeopleListBox.Size = new System.Drawing.Size(332, 352);
             this.listPeopleListBox.TabIndex = 6;
             // 
             // addPersonButton
             // 
-            this.addPersonButton.Location = new System.Drawing.Point(52, 300);
+            this.addPersonButton.Image = ((System.Drawing.Image)(resources.GetObject("addPersonButton.Image")));
+            this.addPersonButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.addPersonButton.Location = new System.Drawing.Point(64, 396);
             this.addPersonButton.Name = "addPersonButton";
-            this.addPersonButton.Size = new System.Drawing.Size(234, 69);
+            this.addPersonButton.Size = new System.Drawing.Size(247, 69);
             this.addPersonButton.TabIndex = 7;
-            this.addPersonButton.Text = "Add Person";
+            this.addPersonButton.Text = "          Add Person";
             this.addPersonButton.UseVisualStyleBackColor = true;
             this.addPersonButton.Click += new System.EventHandler(this.addPersonButton_Click);
             // 
             // refreshListButton
             // 
-            this.refreshListButton.Location = new System.Drawing.Point(52, 387);
+            this.refreshListButton.Image = ((System.Drawing.Image)(resources.GetObject("refreshListButton.Image")));
+            this.refreshListButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.refreshListButton.Location = new System.Drawing.Point(64, 488);
             this.refreshListButton.Name = "refreshListButton";
-            this.refreshListButton.Size = new System.Drawing.Size(234, 69);
+            this.refreshListButton.Size = new System.Drawing.Size(247, 69);
             this.refreshListButton.TabIndex = 8;
-            this.refreshListButton.Text = "Delete Person";
+            this.refreshListButton.Text = "          Delete Person";
             this.refreshListButton.UseVisualStyleBackColor = true;
             this.refreshListButton.Click += new System.EventHandler(this.refreshListButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(54, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(659, 104);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
             // 
             // PeopleForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(754, 492);
+            this.ClientSize = new System.Drawing.Size(764, 582);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.refreshListButton);
             this.Controls.Add(this.addPersonButton);
             this.Controls.Add(this.listPeopleListBox);
@@ -138,6 +156,7 @@
             this.Margin = new System.Windows.Forms.Padding(7, 8, 7, 8);
             this.Name = "PeopleForm";
             this.Text = "SQLite Portable Database";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -154,6 +173,7 @@
         private System.Windows.Forms.ListBox listPeopleListBox;
         private System.Windows.Forms.Button addPersonButton;
         private System.Windows.Forms.Button refreshListButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
